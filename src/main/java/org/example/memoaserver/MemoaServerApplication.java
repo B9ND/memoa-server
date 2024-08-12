2af6b4f146b49f0472d84a466382642ac414bed5
daedyu <134924951+daedyu@users.noreply.github.com>
2024-08-12 20:28:58 +0900

    feat: add spring boot

---
+++ src/main/java/org/example/memoaserver/MemoaServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.memoaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoaServerApplication.class, args);
+    }
+
+}
