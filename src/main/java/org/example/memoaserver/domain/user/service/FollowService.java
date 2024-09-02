7d0c8305f9b9cbd5102c036543ed9b50acc350e0
bestdevmgp <pmg3858@icloud.com>
2024-09-02 19:35:38 +0900

    feat: follow system

--- src/main/java/org/example/memoaserver/domain/user/service/FollowService.java
+++ src/main/java/org/example/memoaserver/domain/user/service/FollowService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.memoaserver.domain.user.dto.FollowDTO;
 import org.example.memoaserver.domain.user.dto.UserDTO;
 import org.example.memoaserver.domain.user.entity.FollowEntity;
-import org.example.memoaserver.domain.user.entity.UserEntity;
 import org.example.memoaserver.domain.user.repository.FollowRepository;
 import org.example.memoaserver.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -15,13 +14,13 @@ public class FollowService {
     private final FollowRepository followRepository;
     private final UserRepository userRepository;
 
-    public void addFollower(UserDTO user, UserDTO follower) {
+    public void addFollower(UserDTO user, FollowDTO follower) {
         Long userId = userRepository.findByEmail(user.getEmail()).getId();
-        Long followId = userRepository.findByEmail(follower.getEmail()).getId();
+        Long followId = userRepository.findByEmail(user.getEmail()).getId();
 
         FollowEntity followEntity = new FollowEntity();
-        followEntity.setId(followId);
-        followEntity.setFollower(userId);
+        followEntity.setFollowing(userId);
+        followEntity.setFollower(followId);
 
         followRepository.save(followEntity);
     }
