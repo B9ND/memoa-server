679ea1b1d91847a93369b14aa3fbacc445d37721
daedyu <134924951+daedyu@users.noreply.github.com>
2024-09-11 20:39:11 +0900

    fix: cors setting

--- src/main/java/org/example/memoaserver/global/config/SecurityConfig.java
+++ src/main/java/org/example/memoaserver/global/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(3600L);
 
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
                         return configuration;
                     }
