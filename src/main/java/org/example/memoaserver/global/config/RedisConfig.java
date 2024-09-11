785f691559c80756bf0d7b0a7915e31af28eeb56
daedyu <134924951+daedyu@users.noreply.github.com>
2024-09-11 21:32:43 +0900

    feat: add lettuce

--- src/main/java/org/example/memoaserver/global/config/RedisConfig.java
+++ src/main/java/org/example/memoaserver/global/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package org.example.memoaserver.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -11,6 +13,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        configuration.setPassword(redisPassword);
+        return new LettuceConnectionFactory(configuration);
+    }
+
     @Bean
     @Primary
     public RedisTemplate<String, Object> redisTemplate0(RedisConnectionFactory redisConnectionFactory) {
