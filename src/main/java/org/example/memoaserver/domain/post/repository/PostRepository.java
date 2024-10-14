343011c67a00be5735cc15e467ac66ddc31cad9d
daedyu <134924951+daedyu@users.noreply.github.com>
2024-10-14 16:35:42 +0900

    docs: add swagger api model in PostReq.java

--- src/main/java/org/example/memoaserver/domain/post/repository/PostRepository.java
+++ src/main/java/org/example/memoaserver/domain/post/repository/PostRepository.java
@@ -5,7 +5,6 @@ import org.example.memoaserver.domain.post.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
     List<PostEntity> findByTitleContainingOrContentContaining(String title, String content);
