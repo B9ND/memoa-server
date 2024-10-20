33f86959f34d1f02c63d32181f008f54b8e4f857
bestdevmgp <pmg3858@icloud.com>
2024-10-20 16:31:41 +0900

    merge: develop

--- src/main/java/org/example/memoaserver/domain/user/service/BookmarkService.java
+++ src/main/java/org/example/memoaserver/domain/user/service/BookmarkService.java
@@ -1,23 +1,23 @@
-//package org.example.memoaserver.domain.user.service;
-//
-//import jakarta.servlet.http.HttpSession;
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import org.example.memoaserver.domain.post.repository.PostRepository;
-//import org.example.memoaserver.domain.post.service.PostService;
-//import org.example.memoaserver.domain.user.dto.UserDTO;
-//import org.example.memoaserver.domain.user.entity.UserEntity;
-//import org.example.memoaserver.domain.user.repository.BookmarkRepository;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class BookmarkService {
-//    private final PostService postService;
-//    private final PostRepository postRepository;
-//    private final BookmarkRepository bookmarkRepository;
-//
-//
-//}
+package org.example.memoaserver.domain.user.service;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.memoaserver.domain.post.repository.PostRepository;
+import org.example.memoaserver.domain.post.service.PostService;
+import org.example.memoaserver.domain.user.dto.UserDTO;
+import org.example.memoaserver.domain.user.entity.UserEntity;
+import org.example.memoaserver.domain.user.repository.BookmarkRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkService {
+    private final PostService postService;
+    private final PostRepository postRepository;
+    private final BookmarkRepository bookmarkRepository;
+
+
+}
