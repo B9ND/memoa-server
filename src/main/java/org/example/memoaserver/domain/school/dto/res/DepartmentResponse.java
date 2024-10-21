57434a30a4447576e3f26fd950c1a1301d5cbe08
daedyu <134924951+daedyu@users.noreply.github.com>
2024-10-21 19:45:53 +0900

    feat: add getMySchool

---
+++ src/main/java/org/example/memoaserver/domain/school/dto/res/DepartmentResponse.java
@@ -0,0 +1,36 @@
+package org.example.memoaserver.domain.school.dto.res;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.example.memoaserver.domain.school.entity.DepartmentEntity;
+import org.example.memoaserver.domain.school.entity.SchoolEntity;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class DepartmentResponse {
+    private String name;
+
+    private Integer grade;
+
+    private SchoolResponse school;
+
+    private List<String> subjects;
+
+    public static DepartmentResponse fromDepartmentEntity(DepartmentEntity departmentEntity) {
+        return DepartmentResponse.builder()
+                .name(departmentEntity.getName())
+                .grade(departmentEntity.getGrade())
+                .school(getSchool(departmentEntity.getSchoolEntity()))
+                .subjects(departmentEntity.getSubjects())
+                .build();
+    }
+
+    private static SchoolResponse getSchool(SchoolEntity schoolEntity) {
+        return SchoolResponse.builder()
+                .name(schoolEntity.getName())
+                .name(schoolEntity.getName())
+                .build();
+    }
+}
