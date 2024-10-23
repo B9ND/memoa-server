51aa41fa3b3f0209152068101f4193bd40e4ce47
bestdevmgp <pmg3858@icloud.com>
2024-10-23 23:54:38 +0900

    perf: BookmarkRequest

--- src/main/java/org/example/memoaserver/domain/bookmark/dto/req/BookmarkRequest.java
+++ src/main/java/org/example/memoaserver/domain/bookmark/dto/req/BookmarkRequest.java
@@ -1,17 +1,14 @@
 package org.example.memoaserver.domain.bookmark.dto.req;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class BookmarkRequest {
     private Long userId;
     private Long postId;
-
-    public BookmarkRequest(Long userId, Long postId) {
-        this.postId = postId;
-        this.userId = userId;
-    }
 }
