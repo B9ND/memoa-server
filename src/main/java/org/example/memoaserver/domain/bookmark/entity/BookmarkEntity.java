465f97a668f37d453db00a04d2fc59dd5c32daf8
bestdevmgp <pmg3858@icloud.com>
2024-10-29 09:03:22 +0900

    docs: remove import

--- src/main/java/org/example/memoaserver/domain/bookmark/entity/BookmarkEntity.java
+++ src/main/java/org/example/memoaserver/domain/bookmark/entity/BookmarkEntity.java
@@ -1,7 +1,6 @@
 package org.example.memoaserver.domain.bookmark.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
