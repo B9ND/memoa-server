ea919ce993ad2f72e807c581f1b24774dcb529ed
daedyu <134924951+daedyu@users.noreply.github.com>
2024-11-18 23:37:16 +0900

    refactor: s3 code

--- src/main/java/org/example/memoaserver/domain/image/service/UploadService.java
+++ src/main/java/org/example/memoaserver/domain/image/service/UploadService.java
@@ -10,6 +10,7 @@ import org.example.memoaserver.domain.image.exception.ImageNotFoundException;
 import org.example.memoaserver.domain.image.exception.ImageTypeException;
 import org.example.memoaserver.domain.image.exception.ImageUploadException;
 import org.example.memoaserver.domain.image.exception.enums.ImageExceptionStatusCode;
+import org.example.memoaserver.domain.image.support.ImageValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,53 +25,32 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class UploadService {
-    private final AmazonS3 amazonS3;
-
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    private void validateImageFileExtension(String filename) {
-        int lastDotIndex = filename.lastIndexOf(".");
-        if (lastDotIndex == -1) {
-            throw new ImageTypeException(ImageExceptionStatusCode.NO_FILE_TYPE);
+    private final AmazonS3 amazonS3;
+    private final ImageValidator imageValidator;
+
+    public ImageResponse upload(MultipartFile image) throws IOException {
+        imageValidator.validateImageFileExtension(Objects.requireNonNull(image.getOriginalFilename()));
+        if (image.isEmpty() || Objects.isNull(image.getOriginalFilename())) {
+            throw new ImageNotFoundException();
         }
-
-        String extension = filename.substring(lastDotIndex + 1).toLowerCase();
-        List<String> allowedExtensionList = Arrays.asList("jpg", "jpeg", "png", "gif", "webp");
-
-        if (!allowedExtensionList.contains(extension)) {
-            throw new ImageTypeException(ImageExceptionStatusCode.UNSUPPORTED_MEDIA_TYPE);
-        }
-    }
-
-    private String getContentType(String extension) {
-        return switch (extension) {
-            case "jpg", "jpeg" -> "image/jpeg";
-            case "png" -> "image/png";
-            case "gif" -> "image/gif";
-            case "webp" -> "image/webp";
-            default -> "application/octet-stream";
-        };
+        return ImageResponse.builder()
+                .url(uploadImageToS3(image))
+                .build();
     }
 
     private String uploadImageToS3(MultipartFile image) throws IOException {
         String originalFilename = image.getOriginalFilename();
-        String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
-
         String s3FileName = UUID.randomUUID().toString().substring(0, 10) + "_" + originalFilename;
-
         byte[] bytes = image.getBytes();
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(getContentType(extension));
-        metadata.setContentLength(bytes.length);
+        ObjectMetadata metadata = createMetadata(originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase(), bytes);
 
-        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)) {
-            PutObjectRequest putObjectRequest =
-                    new PutObjectRequest(bucket, s3FileName, byteArrayInputStream, metadata)
-                            .withCannedAcl(CannedAccessControlList.PublicRead);
-
-            amazonS3.putObject(putObjectRequest);
+        try {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+            amazonS3.putObject(createPutObjectRequest(s3FileName, byteArrayInputStream, metadata));
         } catch (Exception e) {
             throw new ImageUploadException();
         }
@@ -78,14 +58,15 @@ public class UploadService {
         return amazonS3.getUrl(bucket, s3FileName).toString();
     }
 
-    public ImageResponse upload(MultipartFile image) throws IOException {
-        validateImageFileExtension(Objects.requireNonNull(image.getOriginalFilename()));
-        if (image.isEmpty() || Objects.isNull(image.getOriginalFilename())) {
-            throw new ImageNotFoundException();
-        }
+    private PutObjectRequest createPutObjectRequest(String name, ByteArrayInputStream byteStream, ObjectMetadata metadata) {
+        return new PutObjectRequest(bucket, name, byteStream, metadata)
+            .withCannedAcl(CannedAccessControlList.PublicRead);
+    }
 
-        return ImageResponse.builder()
-                .url(uploadImageToS3(image))
-                .build();
+    private ObjectMetadata createMetadata(String extension, byte[] bytes) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(imageValidator.getContentType(extension));
+        metadata.setContentLength(bytes.length);
+        return metadata;
     }
 }
