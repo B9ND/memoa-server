87a12d1d550ae8433be48ccfc28096a38b4e127c
daedyu <134924951+daedyu@users.noreply.github.com>
2024-11-19 23:32:01 +0900

    fix: token expired and payment error

--- src/main/java/org/example/memoaserver/global/exception/enums/TokenExceptionStatusCode.java
+++ src/main/java/org/example/memoaserver/global/exception/enums/TokenExceptionStatusCode.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 public enum TokenExceptionStatusCode implements StatusCode {
     REFRESH_NOT_FOUND(404, "refresh 토큰은 찾을 수 없습니다."),
     INVALID_TOKEN(402, "유요하지 않은 토큰"),
-    TOKEN_EXPIRED(403, "토큰 만료됨"),
+    TOKEN_EXPIRED(401, "토큰 만료됨"),
     CATEGORY_NOT_FOUND(404, "찾을 수 없는 카테고리");
 
     private final int status;
